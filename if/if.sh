b0a534b25591d528239c3d71a43da7b364ac9cc3
FumiyaOgyu <fumiya.ogyu@wata-lab.meijo-u.ac.jp>
2017-03-08 01:18:42 +0900

    if.shの追加

---
+++ if/if.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+find . -name "*.c" | xargs sed -i 's#if(#if\ (#g'
+
+find . -name "*.c" | xargs sed -i 's#for(#for\ (#g'
+
+find . -name "*.c" | xargs sed -i 's#while(#while\ (#g'
+
+find . -name "*.c" | xargs sed -i 's#switch(#switch\ (#g'
